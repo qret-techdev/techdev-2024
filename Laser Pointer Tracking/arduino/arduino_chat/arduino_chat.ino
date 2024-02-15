e84e493d43f53675af8a2e61920fe88f86dadf77
teighinnordholt <teighinnordholt@gmail.com>
2024-02-15 14:17:58 -0500

    increased serial speed

--- Laser Pointer Tracking/arduino/arduino_chat/arduino_chat.ino
+++ Laser Pointer Tracking/arduino/arduino_chat/arduino_chat.ino
@@ -1,11 +1,35 @@
+const int led_red = 2;
+const int led_green = 6;
+
+float speed;
+
 void setup() {
   Serial.begin(115200); // Initialize serial communication
+
+  pinMode(led_red, OUTPUT);
+  pinMode(led_green, OUTPUT);
+
+  writeboth(LOW);
 }
 
 void loop() {
   if (Serial.available() > 0) {
-    float speed = Serial.parseFloat(); // Read speed from serial
-    Serial.print("Received speed: ");
-    Serial.println(speed);
+    speed = Serial.parseFloat(); // Read speed from serial
+    Serial.println("ACK");
+    writeboth(LOW);
   }
+
+  if(speed > 0){
+    writeboth(LOW);
+    digitalWrite(led_green, HIGH);
+  }else if(speed < 0){
+    writeboth(LOW);
+    digitalWrite(led_red, HIGH);
+  }
+  
+}
+
+void writeboth(bool sig){
+  digitalWrite(led_red, sig);
+  digitalWrite(led_green, sig);
 }
--- Laser Pointer Tracking/pyserialtest.py
+++ Laser Pointer Tracking/pyserialtest.py
@@ -1,9 +1,23 @@
 import serial
 import time
 
-ser = serial.Serial('COM10', 115200)
+ser = serial.Serial('COM8', 115200)
 
-speed1 = 45
+speeds = [-45, 45]
+
+temp = 1
 
 while(1):
-    ser.write(f"{speed1:.2f}".encode())
+    if(temp == 1):
+        message = speeds[0]
+        temp = 0
+    else:
+        message = speeds[1]
+        temp = 1
+
+    ser.write(f"{message:.2f}\n".encode())
+
+    print(message)
+    #ser.flush()
+
+    time.sleep(0.02)
