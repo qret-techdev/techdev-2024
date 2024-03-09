0cf814610f55bc7e04f1953847ea51824cd2126a
teighinnordholt <teighinnordholt@gmail.com>
2024-03-08 23:03:43 -0500

    update before first 2 axis test

--- 2dim Test/funcs.py
+++ 2dim Test/funcs.py
@@ -1,5 +1,42 @@
-def get_rock_y(frame):
+def get_rock_y(frame): #pseudo function
     return #pixels from vertical of rocket
 
-def get_rock_x(frame):
+def get_rock_x(frame): #pseudo function
     return #pixels from horizontal of rocket
+
+#IMAGE SUBTRACTION
+
+import cv2 as cv
+import numpy as np
+
+def get_adj_frame(frame, fov, d_theta):
+    shape = frame.shape
+    adjusted_frame = np.zeros(shape)
+    adjusted_frame[int(shape[0] - shape[0]*(fov-d_theta)/fov):, :, :] = frame[:int(shape[0]*(fov-d_theta)/fov), :, :]
+    return adjusted_frame
+
+def subtract_prev_frame(curr_frame, prev_frame, fov, d_theta):
+    #print(np.maximum(curr_frame[0][0] - np.array([168, 128, 98]), np.array([0, 0, 0])))
+    return np.maximum(curr_frame - get_adj_frame(prev_frame, fov, d_theta), np.array([0, 0, 0])).astype(np.uint8)
+    
+def findMax(image):
+    #convert to hsv format
+    grayImage = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+
+    loc = np.unravel_index(grayImage.argmax(), grayImage.shape)[::-1]
+    return loc
+
+def findGradientMax(image, prev_loc, strength=1/4):
+    grayImage = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+    dims = grayImage.shape
+
+    x_axis = np.linspace(-prev_loc[0]/dims[0], 1 - prev_loc[0]/dims[0], dims[0])[:, None]
+    y_axis = np.linspace(-prev_loc[1]/dims[1], 1 - prev_loc[1]/dims[1], dims[1])[None, :]
+
+    subtract_arr = np.sqrt(x_axis ** 2 + y_axis ** 2)
+    multiply_arr = np.ones(dims) - strength*subtract_arr
+
+    gradient_image = grayImage*multiply_arr
+
+    loc = np.unravel_index(gradient_image.argmax(), gradient_image.shape)[::-1]
+    return loc
---
+++ 2dim Test/userinput_2steppers/userinput_2steppers.ino
@@ -0,0 +1,72 @@
+#include <AccelStepper.h>
+
+// Motor 1 Connections
+const int dirPin = 4;
+const int stepPin = 3;
+
+// Motor 2 Connections
+const int dirPin2 = 6;
+const int stepPin2 = 5;
+
+// two stepper motors
+AccelStepper myStepper1(AccelStepper::DRIVER, stepPin, dirPin);
+AccelStepper myStepper2(AccelStepper::DRIVER, stepPin2, dirPin2);
+
+void setup() {
+  // Motor 1 Configuration
+  myStepper1.setMaxSpeed(13000); // Set max speed
+  myStepper1.setSpeed(4000);    // Set initial speed
+  
+  // Motor 2 Configuration
+  myStepper2.setMaxSpeed(13000); //max speed
+  myStepper2.setSpeed(4000);    // initial speed
+  
+  // Microstepping pins configuration
+  pinMode(A0, OUTPUT);
+  pinMode(A1, OUTPUT);
+  pinMode(A2, OUTPUT);
+
+  // Microstepping for 1/32
+  digitalWrite(A0, HIGH);
+  digitalWrite(A1, HIGH);
+  digitalWrite(A2, HIGH);
+
+  Serial.begin(9600);
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    // Expecting input format: speed1 speed2
+    float speed1 = Serial.parseInt(); // Reads the first speed until it encounters a non-integer character
+    float speed2 = Serial.parseInt(); // Reads the next integer after the space
+    Serial.print(speed1);
+    Serial.println(speed2);
+    
+    // Apply the speeds to the stepper motors
+    if (speed1 > 0 && speed2 > 0) {
+      
+      speed1 = (speed1/360)*6400;
+      speed2 = (speed2/360)*6400;
+      
+      myStepper1.setSpeed(speed1);
+      Serial.print("Speed1 set to: ");
+      Serial.print(speed1);
+      myStepper2.setSpeed(speed2);
+      Serial.print(" Speed2 set to: ");
+      Serial.println(speed2);
+    } else {
+      Serial.println("Invalid input. Enter positive numbers separated by a space.");
+    }
+
+    // Clear the serial buffer by reading until newline or timeout
+    while (Serial.available() > 0) {
+      char c = Serial.read();
+      if (c == '\n' || c == '\r') break;
+    }
+  }
+
+  // Run both stepper motors at their set speeds
+
+  myStepper1.runSpeed();
+  myStepper2.runSpeed();
+}
