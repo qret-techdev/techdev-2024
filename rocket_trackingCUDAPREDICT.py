b91b372d723d8713103876a171a142398c677101
Tristan <talderson42@gmail.com>
2024-07-01 18:22:33 -0400

    fixed pidy calculation
    was not using filter y location

--- rocket_trackingCUDAPREDICT.py
+++ rocket_trackingCUDAPREDICT.py
@@ -195,7 +195,7 @@ def main():
 
       #getting motor accelerations using predicted location
       motor_accelx = pidx(loc_x_y_filt[0] + t_delay*(loc_x_y_filt[0]-prevx)/delta_t)
-      motor_accely = -pidy(loc_x_y_filt[0] + t_delay*(loc_x_y_filt[1]-prevy)/delta_t)
+      motor_accely = -pidy(loc_x_y_filt[1] + t_delay*(loc_x_y_filt[1]-prevy)/delta_t)
 
       #updating previous locations
       prevx = loc_x_y_filt[0]
