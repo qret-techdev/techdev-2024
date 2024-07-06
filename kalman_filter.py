b5c6eecf0a08c77b49c8a91e1a90dfbd55c5d223
Tristan <talderson42@gmail.com>
2024-07-06 14:25:11 -0400

    new tracking file with kalman filtering and prediction

--- kalman_filter.py
+++ kalman_filter.py
@@ -6,8 +6,8 @@ class ExampleEKF:
     Vectorized EKF model for estimating the positions of the trackables
     based on noisy x, y coordinates.
     """
-    def __init__(self, simulation):
-        self.n_trackables = len(simulation.trackables)  # Number of trackable objects
+    def __init__(self, n_trackables):
+        self.n_trackables = n_trackables  # Number of trackable objects
         self.STATE_DIM = 2  # State dimension (x, y coordinates)
 
         # Initialize mean state vector and covariance matrix for each trackable
@@ -50,97 +50,3 @@ class ExampleEKF:
         y_lin = observations - simdkalman.primitives.ddot(H, self.m)
         # Update step of EKF
         self.m, self.P = simdkalman.primitives.update(self.m, self.P, H, self.R, y_lin)
-
-
-class Simulation:
-    """
-    Simulate movement of the rocket. This is not vectorized and does not
-    aim to be very efficient.
-    """
-    def __init__(self):
-        class Trackable:
-            def __init__(self):
-                self.position = np.array([0.0, 0.0])
-                self.velocity = np.array([0.5, 1.0])
-
-            def move(self, delta_t):
-                self.position += self.velocity * delta_t
-
-        N_TRACKABLES = 1
-        self.trackables = [Trackable() for _ in range(N_TRACKABLES)]
-        self.time = 0
-
-    def simulate_step(self):
-        DELTA_T = 0.08
-        T_MAX = 20
-        MEASUREMENT_NOISE = 0.1
-
-        self.time += DELTA_T
-        if self.time > T_MAX: return None
-
-        true_positions = []
-        observations = np.zeros((len(self.trackables), 2, 1))
-
-        for obj_i, obj in enumerate(self.trackables):
-            obj.move(DELTA_T)
-            true_positions.append(obj.position)
-
-            noisy_position = obj.position + np.random.normal(scale=MEASUREMENT_NOISE, size=2)
-            observations[obj_i, :, 0] = noisy_position
-
-        return (true_positions, observations)
-
-def uncertainty_ellipse_95(mean, cov):
-    N = 30
-    theta = np.linspace(0, 2*np.pi, num=N)
-    circle = np.vstack([c[np.newaxis, :] for c in (np.sin(theta), np.cos(theta))])
-
-    u, s_vec, _ = np.linalg.svd(cov)
-    scale_for_95 = 2.0 * np.sqrt(5.991)
-    s_mat = np.diag(np.sqrt(s_vec)) * scale_for_95
-
-    return np.dot(u, np.dot(s_mat, circle)).transpose() + mean[np.newaxis, :]
-
-# Initialize
-np.random.seed(100)
-
-simulation = Simulation()
-ekf = ExampleEKF(simulation)
-
-true_trajectories = []
-estimated_trajectories = []
-estimated_uncertainties = []
-
-# Run simulation and estimation
-while True:
-    simulated = simulation.simulate_step()
-    if simulated is None: break
-
-    true_positions, observations = simulated
-    true_trajectories.append(true_positions)
-
-    ekf.predict()
-    ekf.update(observations)
-
-    estimated_trajectories.append(ekf.m[:, :, 0])
-    for i in range(ekf.P.shape[0]):
-        estimated_uncertainties.append(uncertainty_ellipse_95(ekf.m[i, :, 0], ekf.P[i, ...]))
-
-# Visualize results
-import matplotlib.pyplot as plt
-
-for ell in estimated_uncertainties:
-    plt.plot(ell[:, 0], ell[:, 1], color='blue', lw=1, alpha=0.1)
-
-true_trajectories = np.array(true_trajectories)
-estimated_trajectories = np.array(estimated_trajectories)
-for i in range(len(true_trajectories[0])):
-    kwargs = {}
-    if i == 0: kwargs['label'] = 'true trajectories'
-    plt.plot(true_trajectories[:, i, 0], true_trajectories[:, i, 1], 'k', **kwargs)
-    if i == 0: kwargs['label'] = 'estimated trajectories'
-    plt.plot(estimated_trajectories[:, i, 0], estimated_trajectories[:, i, 1], 'bx', alpha=0.5, **kwargs)
-
-plt.legend()
-plt.axis('equal')
-plt.show()
--- trackingPredict_kalman.py
+++ trackingPredict_kalman.py
@@ -5,16 +5,194 @@ import cv2 #getting video, monitoring key presses while in video window
 from simple_pid import PID #control system pid
 import time #measure time, calculate speed from accel and delta t
 import serial #communication with arduino
+import kalman_filter as kf
 
 from ultralytics import YOLO
 from ultralytics.utils.plotting import Annotator, colors
 
+import datetime
+
 AVG_NUMBER = 3
-DEVICE_NUMBER = 1
+DEVICE_NUMBER = 0
 Y_FRAME_SIZE = 640
 X_FRAME_SIZE = 480
-CUDA = 1
 SERIAL = 0
+KALMAN = 1
+WEIGHTS = "weights/best.pt"
 
 if(SERIAL):
   ser = serial.Serial('COM5', 115200) #might have to change com number, ex 'COM11'... best to keep a high baud rate, make sure it matches w/ arduino
+
+if(KALMAN):
+  n_trackables = 1
+  ekf = kf.ExampleEKF(n_trackables)
+
+
+def initialize_mov_avg(size):
+    return np.zeros(size), np.zeros(size)
+#    pidx, pidy
+def initialize_pid():
+    return PID(0.047, 0.0011, 0.10, setpoint=0), PID(0.0444, 0, 0, setpoint=0)
+#   speed, accel, loc_x_y_filt, delta_t, prev_time
+def initialize_motor_variables():
+    return [0, 0], [0, 0], [0, 0], [0, 0], 0, 0
+#   t_delay, prevx, prevy, velx, vely, trip_init_guess, motor_speedy_init_guess
+def initialize_tracking_variables():
+    return 0.4, 0, 0, 0, 0, 0, 0
+
+def initialize_video_writer(cap, width, height, fps):
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+    filename = f"output_video/output_{timestamp}.mp4"
+    return cv2.VideoWriter(filename, fourcc, fps, (width, height))
+
+def update_speed_and_time(speed, accel, delta_t, prev_time):
+    delta_t = time.time() - prev_time
+    speed[0] += accel[0] * delta_t
+    speed[1] += accel[1] * delta_t
+    prev_time = time.time()
+    return speed, delta_t, prev_time
+
+def send_speed_to_arduino(speed):
+    ser.write(f'{speed[0]:.2f}\n'.encode())
+    ser.write(f'{speed[1]:.2f}\n'.encode())
+    ser.flushInput()
+    ser.flushOutput()
+
+def process_center(loc, mov_avg_x, mov_avg_y):
+  loc_rel = loc - np.array((X_FRAME_SIZE/2, Y_FRAME_SIZE/2))
+        
+  #rolling the moving average arrray to get rid of first value
+  mov_avg_x = np.roll(mov_avg_x, -1)
+  mov_avg_y = np.roll(mov_avg_y, -1)
+
+  #replacing oldest value (moved to end with roll) with the newest
+  mov_avg_x[-1] = loc_rel[0]
+  mov_avg_y[-1] = -loc_rel[1]
+
+  #averaging the array
+  rock_x_filt = (sum(mov_avg_x))/AVG_NUMBER
+  rock_y_filt = (sum(mov_avg_y))/AVG_NUMBER
+
+  print(rock_x_filt, rock_y_filt)
+  return (rock_x_filt, rock_y_filt)
+
+
+def process_frame(frame, model, track_history, names, mov_avg_x, mov_avg_y, confidence_threshold=0.2):
+    """
+    Process a single frame for object detection and tracking.
+
+    Parameters:
+    - frame: The current video frame to process.
+    - model: The YOLO model used for object detection.
+    - track_history: A dictionary maintaining track history for each detected object.
+    - names: Class names for detected objects.
+    - confidence_threshold: Minimum confidence level required to process a detection.
+
+    Returns:
+    - frame: The processed frame with annotations.
+    - loc_filt: Filtered location coordinates.
+    """
+    loc = (0, 0)
+    loc_filt = (0, 0)
+    results = model.track(frame, persist=True)
+    boxes = results[0].boxes.xyxy
+    confs = results[0].boxes.conf
+
+    if results[0].boxes.id is not None:
+        clss = results[0].boxes.cls.tolist()
+        track_ids = results[0].boxes.id.int().tolist()
+        annotator = Annotator(frame, line_width=2)
+
+        for box, cls, track_id, conf in zip(boxes, clss, track_ids, confs):
+            if conf >= confidence_threshold:
+                annotator.box_label(box, color=colors(int(cls), True), label=f"{names[int(cls)]} {conf:.2f}")
+                track = track_history[track_id]
+                loc = (((box[0] + box[2]) / 2).cpu().numpy(), ((box[1] + box[3]) / 2).cpu().numpy())
+                loc_filt = process_center(loc, mov_avg_x, mov_avg_y)
+                track.append((int(loc[0]), int(loc[1])))
+                if len(track) > 30:
+                    track.pop(0)
+                points = np.array(track, dtype=np.int32).reshape((-1, 1, 2))
+                cv2.circle(frame, track[-1], 7, colors(int(cls), True), -1)
+                cv2.polylines(frame, [points], isClosed=False, color=colors(int(cls), True), thickness=2)
+
+    return frame, loc_filt
+  
+
+def main():
+    AVG_NUMBER = 10
+    mov_avg_x, mov_avg_y = initialize_mov_avg(AVG_NUMBER)
+    pidx, pidy = initialize_pid()
+    speed, accel, loc_x_y_unfilt, loc_x_y_filt, delta_t, prev_time = initialize_motor_variables()
+    t_delay, prevx, prevy, velx, vely, trip_init_guess, motor_speedy_init_guess = initialize_tracking_variables()
+    max_speed = 90
+    max_accel = 180
+    sys_state = 0
+    track_history = defaultdict(lambda: [])
+    model = YOLO(WEIGHTS)
+    names = model.model.names
+    cap = cv2.VideoCapture(DEVICE_NUMBER)
+    w, h, fps = (int(cap.get(x)) for x in (cv2.CAP_PROP_FRAME_WIDTH, cv2.CAP_PROP_FRAME_HEIGHT, cv2.CAP_PROP_FPS))
+    result = initialize_video_writer(cap, X_FRAME_SIZE, Y_FRAME_SIZE, fps)
+
+    while True:
+        key = cv2.waitKey(1)
+        if key == ord('q'):
+            break
+        if key == ord(' '):
+            sys_state = (sys_state + 1) % 2
+        if key == ord('r'):
+            speed = [0, 0]
+
+        success, frame = cap.read()
+        if not success:
+            break
+        
+        frame = cv2.rotate(frame, cv2.ROTATE_90_COUNTERCLOCKWISE)
+        loc_x_y_unfilt = process_frame(frame, model, track_history, names, mov_avg_x, mov_avg_y)[1]
+        if KALMAN:
+            observations = np.array(loc_x_y_unfilt).reshape(n_trackables, 2, 1)
+            ekf.predict()
+            ekf.update(observations)
+            loc_x_y_filt = ekf.m[:, :, 0].flatten()
+        else:
+            loc_x_y_filt = loc_x_y_unfilt
+        cv2.imshow("Webcam", frame)
+        result.write(frame)
+
+        if sys_state == 0:
+            accel = [0, 0]
+            if key == ord('w'):
+                speed[1] += 5
+            elif key == ord('s'):
+                speed[0] -= 5
+            elif key == ord('a'):
+                speed[1] -= 5
+            elif key == ord('d'):
+                speed[0] += 5
+            delta_t = time.time() - prev_time
+            prev_time = time.time()
+
+        elif sys_state == 1:
+            if trip_init_guess == 0:
+                speed[1] = motor_speedy_init_guess
+                trip_init_guess += 1
+            accel[0] = pidx(loc_x_y_filt[0] + t_delay * (loc_x_y_filt[0] - prevx) / delta_t)
+            accel[1] = -pidy(loc_x_y_filt[1] + t_delay * (loc_x_y_filt[1] - prevy) / delta_t)
+            prevx, prevy = loc_x_y_filt[0], loc_x_y_filt[1]
+            speed, delta_t, prev_time = update_speed_and_time(speed, accel, delta_t, prev_time)
+
+        if SERIAL:
+            send_speed_to_arduino(speed)
+        print(f'\n State: {sys_state} | motor_speedx: {speed[0]:.2f} | motor_speedy: {speed[1]:.2f} | Accelx: {accel[0]:.2f} | Accely: {accel[1]:.2f} | Time Delta {delta_t:.2f}')
+
+    if SERIAL:
+        send_speed_to_arduino([0, 0])
+
+    cap.release()
+    result.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
