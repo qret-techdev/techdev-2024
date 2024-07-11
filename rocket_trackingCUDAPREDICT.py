4d65224e894be205be067a7f8ababe0291dd16f4
Tristan <talderson42@gmail.com>
2024-07-11 07:29:12 -0400

    added SERIAL constant

--- rocket_trackingCUDAPREDICT.py
+++ rocket_trackingCUDAPREDICT.py
@@ -28,6 +28,7 @@ DEVICE_NUMBER = 0
 Y_FRAME_SIZE = 640
 X_FRAME_SIZE = 480
 <<<<<<< HEAD
+<<<<<<< HEAD
 CUDA = 1
 SERIAL = 0
 
@@ -35,6 +36,9 @@ if(SERIAL):
   ser = serial.Serial('COM5', 115200) #might have to change com number, ex 'COM11'... best to keep a high baud rate, make sure it matches w/ arduino
 =======
 >>>>>>> ecc9fe37 (removed CUDA constant)
+=======
+SERIAL = 1
+>>>>>>> 6e61eb48 (added SERIAL constant)
 
 def process_center(loc, mov_avg_x, mov_avg_y):
   loc_rel = loc - np.array((X_FRAME_SIZE/2, Y_FRAME_SIZE/2))
