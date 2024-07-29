15a9d0a1c43640a373a7f18e8f036c0c934a3da5
Tristan <talderson42@gmail.com>
2024-07-28 21:41:32 -0400

    removed check if model stopped tracking

--- tracking_OOP/kalman_filter.py
+++ tracking_OOP/kalman_filter.py
@@ -50,9 +50,7 @@ class KalmanFilterManager:
         """
         Apply the Kalman filter to a new set of observations.
         """
-        if loc_x_y_unfilt != (0,0):
-            observations = np.array(loc_x_y_unfilt).reshape(1, self.OBS_DIM, 1)
+        observations = np.array(loc_x_y_unfilt).reshape(1, self.OBS_DIM, 1)
         self.predict()
-        if loc_x_y_unfilt != (0,0):
-            self.update(observations)
+        self.update(observations)
         return self.m[:, :, 0].flatten()*(2, 2)
